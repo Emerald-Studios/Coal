53ce8c997f5b369340d50957df6d780fb9cb3835
Sebastian Snoer <sebs@sebsa.dk>
2022-05-29 16:40:25 +0200

    Fix line render mode
    Signed-off-by: Sebastian Snoer <sebs@sebsa.dk>

--- coal/src/main/java/dk/sebsa/coal/graph/RenderPipeline.java
+++ coal/src/main/java/dk/sebsa/coal/graph/RenderPipeline.java
@@ -52,6 +52,7 @@ public class RenderPipeline {
         }
 
         // Render the FBO
+        glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
         FBO.renderFBO(app, prevFBO, new Rect(0, 0, 1, 1));
     }
 
